6a876313bb8775c822ba902e77167493b137cbbe
Alexander Stein <AlexanderStein@t-online.de>
2020-12-02 23:55:12 +0100

    Created first draw of a unit test for updateBindings in ClassCluster.

--- ClassCluster/test/test_Calculation.cpp
+++ ClassCluster/test/test_Calculation.cpp
@@ -13,6 +13,8 @@
 #include "Calculation.h"
 #include "particle.h"
 
+#include <algorithm>
+
 
 class CalculationTest : public ::testing::Test
 {
@@ -40,15 +42,20 @@ public:
 
 protected:
 
-    bool bindsParticleTo(Particle* P1, Particle* P2)
+    bool bindsParticleTo(Particle* P1, Particle* P2) const
     {
         for (int n=0; n<4; ++n) if (P1->bound[n] == P2) return true;
         return false;
     }
 
-    bool areParticlesBound(Particle* P1, Particle* P2)
+    bool areParticlesBound(Particle* P1, Particle* P2) const
     {
         return (bindsParticleTo(P1, P2) && bindsParticleTo(P2, P1));
+    }
+
+    bool areParticlesBound(const int index1, const int index2) const
+    {
+        return areParticlesBound(Calc.P + index1, Calc.P + index2);
     }
 
     bool areParticleBindingsCorrectlyInitialized(int index)
@@ -57,6 +64,18 @@ protected:
              && areParticlesBound(Calc.P + index, Calc.P + index + 2)
              && areParticlesBound(Calc.P + index, Calc.P + index - 2 * Calc.PXS)
              && areParticlesBound(Calc.P + index, Calc.P + index + 2 * Calc.PXS));
+    }
+
+    void swapParticlePositions(const int index1, const int index2)
+    {
+        std::swap(Calc.P[index1].X, Calc.P[index2].X);
+        std::swap(Calc.P[index1].Y, Calc.P[index2].Y);
+        std::swap(Calc.P[index1].Z, Calc.P[index2].Z);
+    }
+
+    void updateBindings()
+    {
+        Calc.updateBindings();
     }
 
     int getPXS()
@@ -80,4 +99,28 @@ TEST_F(CalculationTest, CheckParticleBindingInitialisation)
         EXPECT_TRUE(areParticleBindingsCorrectlyInitialized(index)) << "index = " << index;
         EXPECT_TRUE(areParticleBindingsCorrectlyInitialized(index + 1)) << "index + 1 = " << (index + 1);
     }
+}
+
+TEST_F(CalculationTest, CheckParticleBindingUpdates)
+{
+    int indexRowLength = 2 * getPXS();
+    swapParticlePositions(indexRowLength + 2, indexRowLength + 4);
+    swapParticlePositions(10 * indexRowLength, 11 * indexRowLength);
+    updateBindings();
+
+    EXPECT_TRUE(areParticlesBound(indexRowLength + 4, indexRowLength));
+    EXPECT_TRUE(areParticlesBound(indexRowLength + 4, indexRowLength + 4));
+    EXPECT_TRUE(areParticlesBound(indexRowLength + 4, 2));
+    EXPECT_TRUE(areParticlesBound(indexRowLength + 4, 2 * indexRowLength + 2));
+    EXPECT_TRUE(areParticlesBound(indexRowLength + 2, indexRowLength + 6));
+    EXPECT_TRUE(areParticlesBound(indexRowLength + 2, 4));
+    EXPECT_TRUE(areParticlesBound(indexRowLength + 2, 2 * indexRowLength + 4));
+
+    EXPECT_TRUE(areParticlesBound(11 * indexRowLength + 2, 10 * indexRowLength));
+    EXPECT_TRUE(areParticlesBound(11 * indexRowLength + 2, 10 * indexRowLength + 4));
+    EXPECT_TRUE(areParticlesBound(11 * indexRowLength + 2, 9 * indexRowLength + 2));
+    EXPECT_TRUE(areParticlesBound(11 * indexRowLength + 2, 11 * indexRowLength + 2));
+    EXPECT_TRUE(areParticlesBound(10 * indexRowLength + 2, 11 * indexRowLength));
+    EXPECT_TRUE(areParticlesBound(10 * indexRowLength + 2, 11 * indexRowLength + 4));
+    EXPECT_TRUE(areParticlesBound(10 * indexRowLength + 2, 12 * indexRowLength + 2));
 }
