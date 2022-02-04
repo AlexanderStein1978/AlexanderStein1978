cb4f6fdaec8b44a1e57aac61a7a480a6f1beeda3
Alexander Stein <AlexanderStein@t-online.de>
2022-02-04 23:55:31 +0100

    ClassCluster: Continued to implemented client and server functionality. Compilation not tested.

--- ClassCluster/Network/network.cpp
+++ ClassCluster/Network/network.cpp
@@ -1,10 +1,11 @@
 #include "include/network.h"
+#include <QTcpSocket>
 
 
 int Network::SIZE_OF_COMMAND_STRINGS = 8;
 
 
-Network::Network(Window *window) : minimumDataToRead(1), mSocket(nullptr), mWindow(window),
+Network::Network(Window *window) : minimumDataToRead(SIZE_OF_COMMAND_STRINGS), mSocket(nullptr), mWindow(window),
     mCommandMap({"STARTSTA", "STOPSTOP", "RESETRES", "MOVEMOVE", "TRIGSNAP", "WRITSNAP", "RESTSNAP", "SETKINEN", "SETPOTRS", "SETSPEED", "SETSTEPS", "RELOAPOT", "STOPCALC",
                  "ROTATERO", "SETLAYDI", "DATRECED", "DATFOLLO", "ERRORINC", "ERRORUNK"}),
     continueRunning(true)
@@ -23,7 +24,7 @@ void Network::run()
     if (nullptr == mSocket) return;
     while (continueRunning)
     {
-        if (mSocket->bytesAvailable() >= SIZE_OF_COMMAND_STRINGS) dataReceived();
+        if (mSocket->bytesAvailable() >= minimumDataToRead) dataReceived();
         msleep(1);
     }
 }
@@ -39,3 +40,41 @@ void Network::SendCommand(const Command command)
     SendCommand(mCommandMap.key(command));
 }
 
+void Network::dataReceived()
+{
+    char buffer[SIZE_OF_COMMAND_STRINGS + 1];
+    memset(buffer, 0, SIZE_OF_COMMAND_STRINGS + 1);
+    quint64 bytesRead = mSocket->readData(buffer, SIZE_OF_COMMAND_STRINGS);
+    bool complete = true;
+    if (bytesRead < SIZE_OF_COMMAND_STRINGS)
+    {
+        SendCommand(ERROR_INCOMPLETE);
+        return;
+    }
+    Command command(mCommandMap.value(buffer, ERROR_UNKNOWN_COMMAND));
+    commandReceived(command);
+}
+
+double Network::readDouble(bool complete)
+{
+    double value;
+    bytesRead = mSocket->readData(reinterpret_cast<char*>(&value), sizeof(double));
+    if (bytesRead < sizeof(double))
+    {
+        SendCommand(ERROR_INCOMPLETE);
+        complete = false;
+    }
+    return value;
+}
+
+quint32 Network::readUint32(bool complete)
+{
+    quint32 size;
+    bytesRead = mSocket->readData(reinterpret_cast<char*>(&size), sizeof(quint32));
+    if (bytesRead < sizeof(quint32))
+    {
+        SendCommand(ERROR_INCOMPLETE);
+        complete = false;
+    }
+    return size;
+}
