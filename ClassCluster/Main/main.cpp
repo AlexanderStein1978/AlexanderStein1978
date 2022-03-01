87f36420c8f29c3c95ea101ad6263674d30896d4
Alexander Stein <AlexanderStein@t-online.de>
2022-03-01 02:35:00 +0100

    ClassCluster: Finished the first draw of the logger.

--- ClassCluster/Main/main.cpp
+++ ClassCluster/Main/main.cpp
@@ -2,14 +2,21 @@
 
 #include "controlwindow.h"
 #include "MainWindow.h"
+#include "logfunction.h"
+#include "logwindow.h"
+#include "logger.h"
 
 
 int main(int argc, char **argv)
 {
+    qInstallMessageHandler(LogFunction);
     QApplication app(argc, argv);
     MainWindow MW;
     MW.show();
     MW.showMDIChild(new ControlWindow(&MW));
+    LogWindow* logwindow = new LogWindow(&MW);
+    MW.showMDIChild(logwindow);
+    Logger::getLogger().SetLogWindow(logwindow);
     app.connect(&app, SIGNAL(lastWindowClosed()), &app, SLOT(quit()));
     return app.exec();
 }
