8aae431b8a9abf0335da026e47a20c8511ebc6dd
Alexander Stein <AlexanderStein@t-online.de>
2023-11-10 22:08:35 +0100

    ClassCluster: Almost finished the first draw of the functionality to calculate the deviation depending on the angle.

--- ClassCluster/FitExecMain/fitexeccontrol.h
+++ ClassCluster/FitExecMain/fitexeccontrol.h
@@ -1,7 +1,12 @@
 #ifndef FITEXECCONTROL_H
 #define FITEXECCONTROL_H
 
+
 #include <QObject>
+#include <QMutex>
+
+#include "potstruct.h"
+#include "Calculation.h"
 
 
 class Calculation;
@@ -16,12 +21,18 @@ public:
 
 private slots:
     void printCalcState(int instanceId, int iteration, double currentYCenterDev, double maxYCenterDev);
-    void SaveEnergies(double T, double E);
+    void initInstance(int instanceId);
 
 private:
+    void saveResults();
+
     bool stopped[6] = {false, false, false, false, false, false};
-    double max[6];
+    //double max[6];
+    PotStruct struc[Calculation::NumPot];
+    double angles[100], **results, startE[6];
+    int currentIndex = -1, instanceIndex[6];
     Calculation* Calc[6];
+    QMutex mutex;
 };
 
 #endif // FITEXECCONTROL_H
