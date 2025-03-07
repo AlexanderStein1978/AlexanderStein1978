59f8a98a1d52f4ef15e6329a388ed9beef8ca47f
Alexander Stein <AlexanderStein@t-online.de>
2025-03-07 02:14:54 +0100

    SoudDraw: Created a draw test for the oscillator and fixed a bug.

--- Sound/DrawSound/oscillator.cpp
+++ Sound/DrawSound/oscillator.cpp
@@ -6,7 +6,7 @@
 const double Oscillator::HALF = 0.5;
 
 
-Oscillator::Oscillator() : mHOsq(0.0), mLastAmp(0.0), mLastV(0.0), mAmpAmp0Term(0.0), mAmpV0Term(0.0), mVAmp0Term(0.0), mVV0Term(0.0)
+Oscillator::Oscillator() : mHOsq(0.0), mLastAmp(0.0), mLastV(0.0), mAmpAmp0Term(0.0), mAmpV0Term(0.0), mVAmp0Term(0.0), mVV0Term(0.0), mOmega(0.0), mGamma(0.0)
 {
 }
 
@@ -16,8 +16,10 @@ void Oscillator::initialize(const double omega, const double gamma, const double
     mHOsq = HALF * omega * omega;
     mAmpAmp0Term = expGammaT * (coswt + gammaDOmega * sinwt);
     mAmpV0Term = expGammaT * dOmega * sinwt;
-    mVAmp0Term = expGammaT * (omega + gamma * gammaDOmega) * sinwt;
+    mVAmp0Term = -1.0 * expGammaT * (omega + gamma * gammaDOmega) * sinwt;
     mVV0Term = expGammaT * (coswt - gammaDOmega * sinwt);
+    mOmega = omega;
+    mGamma = gamma;
 }
 
 double Oscillator::newValue(const double deltaAmplitude)
@@ -26,4 +28,9 @@ double Oscillator::newValue(const double deltaAmplitude)
     mLastV = mLastAmp * mVAmp0Term + mLastV * mVV0Term;
     mLastAmp = currAmp - deltaAmplitude;
     return HALF * mLastV * mLastV + mHOsq * mLastAmp * mLastAmp;
+}
+
+double Oscillator::getAmplitudeFor(const double t) const
+{
+    return exp(-1.0 * mGamma * t) * (mLastAmp * cos(mOmega * t) + (mLastAmp * mGamma + mLastV) * sin(mOmega * t) / mOmega);
 }
