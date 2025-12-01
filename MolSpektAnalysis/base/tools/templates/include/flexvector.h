4ba096abccc0f71045be26e1397eee8daef59569
Alexander Stein <AlexanderStein@t-online.de>
2025-12-01 04:42:37 +0100

    Restored working mail address in copyright headers.

--- MolSpektAnalysis/base/tools/templates/include/flexvector.h
+++ MolSpektAnalysis/base/tools/templates/include/flexvector.h
@@ -1,5 +1,5 @@
 //
-// Author: Alexander Stein <webmaster@alexandersteinchanneler1978.com>, (C) 2025
+// Author: Alexander Stein <AlexanderStein@t-online.de>, (C) 2025
 //
 // Copyright: See README file that comes with this source code
 //
